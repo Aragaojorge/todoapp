7017164288349dc8230dfa75a0657690e198f28c
Aragajorge <3glasser@gmail.com>
2024-09-26 23:19:02 -0300

    Add navigation bar.
    Create JSP Fragments.
    
    Set Spring Security.

--- todoapp/src/main/java/com/devjola/springboot/todoapp/todo/TodoController.java
+++ todoapp/src/main/java/com/devjola/springboot/todoapp/todo/TodoController.java
@@ -3,6 +3,8 @@ package com.devjola.springboot.todoapp.todo;
 import java.time.LocalDate;
 import java.util.List;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
@@ -28,7 +30,8 @@ public class TodoController {
 	@RequestMapping("list-todos")
 	public String listAllTodos(ModelMap model) {
 		
-		List<Todo> todos = todoService.findByUsername("devjola");
+		String username = getLoggedInUsername(model);
+		List<Todo> todos = todoService.findByUsername(username);
 		model.addAttribute("todos", todos);
 		return "listTodos";
 		
@@ -37,7 +40,7 @@ public class TodoController {
 	@RequestMapping(value="add-todo", method = RequestMethod.GET)
 	public String showNewTodoPage(ModelMap model) {
 
-		String username = (String)model.get("name"); 
+		String username = getLoggedInUsername(model); 
 		Todo todo = new Todo(0, username, "", LocalDate.now().plusYears(1), false);
 		model.put("todo", todo);
 		return "todo";
@@ -53,7 +56,7 @@ public class TodoController {
 			
 		}
 		
-		String username = (String)model.get("name");
+		String username = getLoggedInUsername(model);
 		todoService.addTodo(username, todo.getDescription(), todo.getTargetDate(), false);
 		return "redirect:list-todos";
 		
@@ -85,11 +88,16 @@ public class TodoController {
 			
 		}
 		
-		String username = (String)model.get("name");
+		String username = getLoggedInUsername(model);
 		todo.setUsername(username);
 		todoService.updateTodo(todo);
 		return "redirect:list-todos";
 		
+	}
+	
+	private String getLoggedInUsername(ModelMap model) {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		return authentication.getName();
 	}
 	
 }
--- todoapp/src/main/resources/META-INF/resources/WEB-INF/jsp/welcome.jsp
+++ todoapp/src/main/resources/META-INF/resources/WEB-INF/jsp/welcome.jsp
@@ -1,14 +1,7 @@
-<html>
-	<head>
-		<title>Welcome Page</title>
-		<link href="webjars/bootstrap/5.3.2/css/bootstrap.min.css" rel="stylesheet">
-	</head>
-	<body>
-		<div class="container">
-			<h2>Welcome ${name} to my page</h2>
-			<a href="list-todos">Manage</a> your todos
-		</div>
-		<script src="webjars/bootstrap/5.3.2/js/bootstrap.min.js"></script>
-		<script src="webjars/jquery/3.6.4/js/jquery.min.js"></script>
-	</body>
-</html>
+<%@ include file="common/header.jspf"%>
+<%@ include file="common/navigation.jspf"%>
+<div class="container">
+	<h2>Welcome ${name} to my page</h2>
+	<a href="list-todos">Manage</a> your todos
+</div>
+<%@ include file="common/footer.jspf"%>
